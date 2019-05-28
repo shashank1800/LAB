494f68ad0f3769058751be876f2da17179686341
Shashank Bhat <shashankbhat1800@gmail.com>
2019-05-28 19:41:27 +0530

    Update SSLAB.c

--- SSLAB.c
+++ SSLAB.c
@@ -377,137 +377,129 @@ int i=0;
 char input[20];
 char stack[20];
 char action[20];
+
 void printfn()
 {
-        int j;
-        for(j=0;j<=top;j++)
-                printf("%c",stack[j]);
-        printf("\t%s",input);
-        printf("\t%s",action);
-        printf("\n");
+	int j;
+    for(j=0;j<=top;j++)
+    	printf("%c",stack[j]);
+	printf("\t\t%s",input);
+	printf("\t\t%s\n",action);
 }
-void check()
-{
-        while(1)
-        {                     
-                if(stack[top]=='T' && stack[top-1]=='+' && stack[top-2]=='E')
-                {
-                        if(input[i+1]=='*')
-                                break;
-                }
-             	if(stack[top]=='T')
-                {
-                       if(input[i+1]=='*')
-                               break;
-                }
-                if(stack[top]=='F' && stack[top-1]=='*' && stack[top-2]=='T')
-                {
-                        strcpy(action,"Reduce T->T*F");
-                        stack[--top]=' ';
-                        stack[--top]='T';
-                        printfn();
-                }
-                if(stack[top]=='T' && stack[top-1]=='+' && stack[top-2]=='E')
-                {
-                       strcpy(action,"Reduce E->E+T");
-                       stack[--top]=' ';
-                       stack[--top]='E';
-                       printfn();
-                }  
-			    if(stack[top]==')' && stack[top-1]=='E' && stack[top-2]=='(')
-			    {
-			   		 strcpy(action,"Reduce F->(E)");
-			   		 stack[--top]=' ';
-                     stack[--top]='F';
-                     printfn();
-			    }
-			                 
-                if(stack[top]=='d' && stack[top-1]=='i')
-                {
-                        strcpy(action,"Reduce F->id");
-                        stack[--top]='F';
-                        printfn();                       
-                }       
-             	else if(stack[top]=='F')
-                {
-                       strcpy(action,"Reduce T->F");
-                       stack[top]='T';
-                       printfn();
-                }
-                else if(stack[top]=='T')
-                {
-                       strcpy(action,"Reduce E->T");
-                       stack[top]='E';
-                       printfn();
-                }
-                else if(stack[top]=='E' || stack[top]=='*' || stack[top]=='+' || stack[top]=='(' || stack[top]==')')
-                {
-                        break;
-                }              
-        }
+
+void check(){
+
+	while(1){
+		if(stack[top] =='T' && stack[top-1]=='+' && stack[top-2]=='E' && input[i+1]!='*')
+		{
+			stack[--top]=' ';
+			stack[--top]='E';
+			strcpy(action,"E->E+T");
+			printfn();
+			check();
+		}
+		if(stack[top] =='F' && stack[top-1]=='*' && stack[top-2]=='T' && input[i+1]!='*')
+		{
+			stack[--top]=' ';
+			stack[--top]='T';
+			strcpy(action,"E->T+F");
+			printfn();
+			check();
+		}
+		if(stack[top]==')' && stack[top-1]=='E' && stack[top-2]=='('){
+			stack[--top]=' ';
+			stack[--top]='F';
+			strcpy(action,"F->(E)");
+			printfn();
+			check();
+		}
+		if(stack[top]=='d' && stack[top-1]=='i'){
+			stack[--top]='F';
+			strcpy(action,"F->id");
+			printfn();
+			check();
+		}
+		if(stack[top]=='F'){
+			stack[top]='T';
+			strcpy(action,"T->F");
+			printfn();
+			check();
+		}
+		if(stack[top]=='T' && input[i+1]!='*' ){
+			stack[top]='E';
+			strcpy(action,"E->T");
+			printfn();
+			check();
+		}
+		else
+			break;
+	}
 }
-int main()
-{
-        printf("Enter the sentence\n");
-        scanf("%s",input);
-        int len=strlen(input);
-        input[len] ='$';
-        stack[++top]='$';
-        printf("Stack\tInput\tAction\n");
-        printf("$\t%s\t---\n",input);
-        for(i=0;input[i]!='$';i++)
-        {
-                if(input[i]=='i' && input[i+1]=='d')
-                {
-                        input[i]=' ';
-                        input[i+1] =' ';
-                        stack[++top] ='i';
-                        stack[++top]='d';
-                        i=i+1;
-                        strcpy(action,"Shift id");
-                        printfn();
-                        check();
-                }
-               else if(input[i]=='*')
-                {
-                        input[i]=' ';
-                        stack[++top]='*';                        
-                        strcpy(action,"Shift *");
-                        printfn();
-                        check();                        
-                }
-               else if(input[i]=='+')
-               {
-                       input[i]=' ';
-                       stack[++top]='+';
-                       strcpy(action,"Shift +");
-                       printfn();
-                       check();
-               }
-               else if(input[i]=='('){
-               		    input[i]=' ';
-                       stack[++top]='(';
-                       strcpy(action,"Shift (");
-                       printfn();
-                       check();
-               }
-               else if(input[i]==')'){
-               			input[i]=' ';
-                       stack[++top]=')';
-                       strcpy(action,"Shift )");
-                       printfn();
-                       check();
-               }
-               else{
-               	        break;
-               }
-        }
-        if(stack[top]=='E' && input[i]=='$')
-        	strcpy(action,"ACCEPT");
-        else
-        	strcpy(action,"REJECT");
-        printfn();
-        return 0;
+
+void main(){
+	
+	int len;
+	printf("Enter the input expression\n");
+	scanf("%s",input);
+	
+	len = strlen(input);
+	input[len]='$';
+	stack[++top] = '$';
+	
+	printf("Stack\t\t Input \t\t Action\n");
+	printf("$\t\t %s \t\t --\n",input);
+	
+	for(i=0;input[i]!='$';i++)
+	{
+		if(input[i]=='i' && input[i+1]=='d'){
+			input[i] =' ';
+			input[i+1]= ' ';
+			i++;
+			stack[++top] = 'i';
+			stack[++top] = 'd';
+			strcpy(action,"Shift id");
+			printfn();
+			check();
+			
+		}
+		if(input[i]=='*'){
+			input[i] =' ';
+			stack[++top] = '*';
+			strcpy(action,"Shift *");
+			printfn();
+			check();
+		}
+		if(input[i]=='+'){
+			input[i] =' ';
+			stack[++top] = '+';
+			strcpy(action,"Shift +");
+			printfn();
+			check();
+		}
+		if(input[i]=='('){
+			input[i] =' ';
+			stack[++top] = '(';
+			strcpy(action,"Shift (");
+			printfn();
+			check();
+		}
+		if(input[i]==')'){
+			input[i] =' ';
+			stack[++top] = ')';
+			strcpy(action,"Shift )");
+			printfn();
+			check();
+		}
+		
+	}
+	
+	if(stack[top] =='E' && input[i] =='$'){
+		strcpy(action,"Accept");
+		printfn();
+	}else{
+		strcpy(action,"Reject");
+		printfn();
+	}
 }
 
 Output:
